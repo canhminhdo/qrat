b864f26380f4e808460f315e8e885d568cbd5e56
Canh Do Minh <minhcanh99@gmail.com>
2025-01-06 12:49:30 +0900

    License preparation

--- cmake/ProjectSettings.cmake
+++ cmake/ProjectSettings.cmake
@@ -10,24 +10,24 @@ set(BUILD_QRAT_DOC
 
 MACRO (TODAY RESULT)
     IF (WIN32)
-        EXECUTE_PROCESS(COMMAND "date" OUTPUT_VARIABLE ${RESULT})
+        execute_process(COMMAND "date" OUTPUT_VARIABLE ${RESULT})
         string(REGEX REPLACE "\n" "" ${RESULT} ${${RESULT}})
     ELSEIF(UNIX)
-        EXECUTE_PROCESS(COMMAND "date" "+%b %d %Y %T" OUTPUT_VARIABLE ${RESULT})
+        execute_process(COMMAND "date" "+%b %d %Y %T" OUTPUT_VARIABLE ${RESULT})
         string(REGEX REPLACE "\n" "" ${RESULT} ${${RESULT}})
     ELSE (WIN32)
-        MESSAGE(SEND_ERROR "date not implemented")
-        SET(${RESULT} "unknown")
+        message(SEND_ERROR "date not implemented")
+        set(${RESULT} "unknown")
     ENDIF (WIN32)
 ENDMACRO (TODAY)
 
-TODAY(RESULT)
+today(RESULT)
 set(PROJECT_BUILD_DATE ${RESULT})
 set(PROJECT_BUGREPORT "canhdo@jaist.ac.jp")
 
 # generate a header file with the information from the project
 configure_file(
-        ${PROJECT_SOURCE_DIR}/include/config.h.in
-        ${PROJECT_BINARY_DIR}/include/config.h
-        @ONLY
+    ${PROJECT_SOURCE_DIR}/include/config.h.in
+    ${PROJECT_BINARY_DIR}/include/config.h
+    @ONLY
 )
