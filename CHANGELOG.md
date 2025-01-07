211e1a920a334fecc26c6894b5288a6b7c054b9c
Canh Minh Do <minhcanh99@gmail.com>
2025-01-07 10:28:44 +0900

    Add AUTHORS and LICENSE

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,14 @@
 ## [1.0.0] - 2025-01-06
+
 ### Added
-- First stable release of QRAT - the reachability analysis tool for quantum programs.
+
+- First stable release of the project
+- Support a wide range of quantum gates
+- Support a rich search command for analyzing quantum programs
+- Available commands:
+    + `load <progFile> .`
+    + `search {[ bound {, depth} ]} in <progId> with <searchType> such that <condition> .`
+    + `show path <number> .`
+    + `set show timming <on|off> .`
+    + `set random seed <number> .`
+    + `quit .`
--- cmake/PackageAddTests.cmake
+++ cmake/PackageAddTests.cmake
@@ -3,8 +3,7 @@ macro(PACKAGE_ADD_TESTS testname linklibs)
         # create an executable in which the tests will be stored
         add_executable(${testname} ${ARGN})
         # link the Google test infrastructure and a default main function to the test executable.
-        target_link_libraries(${testname} PRIVATE ${linklibs} gmock gtest_main MQT::ProjectOptions
-            MQT::ProjectWarnings)
+        target_link_libraries(${testname} PRIVATE ${linklibs} gmock gtest_main)
         # discover tests
         gtest_discover_tests(
             ${testname}
