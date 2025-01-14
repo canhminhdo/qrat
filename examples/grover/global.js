3e4d43718c4edb670b342d04603440c6276a8a64
Canh Minh Do <minhcanh99@gmail.com>
2025-01-14 17:32:18 +0900

    Update grover search generator

--- examples/grover/global.js
+++ examples/grover/global.js
@@ -1,4 +1,8 @@
-targetState = "01101011010101101101";
+targetState5  = "01101";
+targetState10 = "0110101101";
+targetState15 = "011010110101011";
+targetState20 = "01101011010101101101";
+targetState = targetState20;
 nqubits = targetState.length;
 nIters = Math.floor(Math.PI / 4 * Math.sqrt(Math.pow(2, nqubits)));
 
