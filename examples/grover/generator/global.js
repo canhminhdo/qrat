66dcc798ec382e7dd6827af4355e39f29b3b2884
Canh Minh Do <minhcanh99@gmail.com>
2025-05-12 17:33:28 +0900

    update global.js

--- examples/grover/generator/global.js
+++ examples/grover/generator/global.js
@@ -1,7 +1,7 @@
-targetState5  = "01101";
-targetState10 = "0110101101";
-targetState15 = "011010110101011";
-targetState20 = "01101011010101101101";
+targetState5  = "11000";
+targetState10 = "0101101001";
+targetState15 = "110000001101010";
+targetState20 = "00001010111011000000";
 targetState = targetState20;
 nqubits = targetState.length;
 nIters = Math.floor(Math.PI / 4 * Math.sqrt(Math.pow(2, nqubits)));
