8fad324c1a46da2b78b26ac70bbdb991d2c027ae
Canh Minh Do <minhcanh99@gmail.com>
2026-01-16 12:47:29 +0900

    Update Grover with MCZ instead of using H.MCX.H

--- examples/grover/generator/diffusion.js
+++ examples/grover/generator/diffusion.js
@@ -9,14 +9,15 @@ let diffusion = (nqubits) => {
     for (i = 0; i < nqubits; i++) {
         prog += "\tX[q" + i + "];\n";
     }
-    // Apply multi-controlled Z gate
-    prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
+    // Apply multi-controlled Z gate (MCZ = H MCX H)
+    // prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
     let qubits = "";
     for (i = 0; i < nqubits; i++) {
         qubits += "q" + i + ((i == (nqubits - 1)) ? "" : ", ");
     }
-    prog += "\tMCX[" + qubits + "];\n";
-    prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
+    prog += "\tMCZ[" + qubits + "];\n";
+    // prog += "\tMCX[" + qubits + "];\n";
+    // prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
 
     // qc.x(range(n))
     for (i = 0; i < nqubits; i++) {
--- examples/grover/generator/oracle.js
+++ examples/grover/generator/oracle.js
@@ -7,14 +7,15 @@ let oracle = (nqubits, targetState) => {
             prog += "\tX[q" + i + "];\n";
     }
 
-    // Step 2: Apply a multi-controlled Z gate (MCZ)
-    prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
+    // Step 2: Apply a multi-controlled Z gate (MCZ = H MCX H)
+    // prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
     let qubits = "";
     for (i = 0; i < nqubits; i++) {
         qubits += "q" + i + ((i == (nqubits - 1)) ? "" : ", ");
     }
-    prog += "\tMCX[" + qubits + "];\n";
-    prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
+    prog += "\tMCZ[" + qubits + "];\n";
+    // prog += "\tMCX[" + qubits + "];\n";
+    // prog += "\tH[" + "q" + (nqubits - 1) + "];\n";
 
     // Step 3: Undo X gates
     for (i = 0; i < nqubits; i++) {
